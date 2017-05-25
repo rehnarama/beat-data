13a205da6f14ff2187b916a8a963a63d9e5bf4ca
Michael <rehn.michael@outlook.com>
2017-05-25 12:37:05 +0200

    Now uses log-scale, also highlights switch frequencies

--- ampl_over_freq.R
+++ ampl_over_freq.R
@@ -22,12 +22,29 @@ C_high = 10 * 10 ^ (-9)
 R_low = 560 * 10 ^ (3)
 C_low = 1 * 10 ^ (-9)
 
-freqs = 1:5000
+freqs = 1:20000
 
 high_amp = highpass(freqs, R_high, C_high)
 low_amp = lowpass(freqs, R_low, C_low)
 
+high_freq = 100
+low_freq = 300
+
+band_amp_lower = high_amp[high_freq] * low_amp[high_freq]
+band_amp_upper = low_amp[low_freq] * high_amp[low_freq]
+
 layout(matrix(c(1, 1, 2, 3), 2, 2, byrow = TRUE))
-plot(freqs, high_amp * low_amp, type = "l", main="Bandpass-filter", xlab="Frekvens (Hz)", ylab="Förstärkning")
-plot(freqs, high_amp, type = "l", main="Högpass-filter", xlab="Frekvens (Hz)", ylab="Förstärkning")
-plot(freqs, low_amp, type = "l", main="Låspass-filter", xlab="Frekvens (Hz)", ylab="Förstärkning")
+
+plot(freqs, high_amp * low_amp, type = "l", main="Bandpass-filter", xlab="Frekvens (Hz)", ylab="F�rst�rkning", log="x")
+lines(c(high_freq, high_freq), c(0, band_amp_lower), col="blue") # Highlight switch frequencies
+lines(c(low_freq, low_freq), c(0, band_amp_upper), col="blue") # Highlist switch frequencies
+axis(1, at=high_freq) # Make sure label is drawn at switch frequency
+axis(1, at=low_freq) # Make sure label is drawn at switch frequency
+
+plot(freqs, high_amp, type = "l", main="H�gpass-filter", xlab="Frekvens (Hz)", ylab="F�rst�rkning", log="x")
+lines(c(high_freq, high_freq), c(0, high_amp[high_freq]), col="blue") # Highlight switch frequencies
+axis(1, at=high_freq) # Make sure label is drawn at switch frequency
+
+plot(freqs, low_amp, type = "l", main="L�gpass-filter", xlab="Frekvens (Hz)", ylab="F�rst�rkning", log="x")
+lines(c(low_freq, low_freq), c(0, low_amp[low_freq]), col="blue") # Highlist switch frequencies
+axis(1, at=low_freq) # Make sure label is drawn at switch frequency
